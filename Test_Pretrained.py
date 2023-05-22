651a02ad7c76106d18de9ea0bad30956ea115369
BianChang <519388527@qq.com>
2023-05-22 18:50:46 +0100

    0522

--- Test_Pretrained.py
+++ Test_Pretrained.py
@@ -1,6 +1,6 @@
 import torch
 from SwinVisionTranformer import SwinTransformer
-
+'''
 import timm
 
 # Load the pre-trained model
@@ -9,7 +9,7 @@ model = timm.create_model('swin_tiny_patch4_window7_224', pretrained=True)
 # Print the names and structure of the layers
 for name, param in model.named_parameters():
     print(name)
-
+'''
 '''
 from transformers import AutoImageProcessor, UperNetForSemanticSegmentation
 from PIL import Image
@@ -39,3 +39,64 @@ for name, param in swin_t.named_parameters():
 # Freeze the encoder's weights
 # for param in swin_t.parameters():
     # param.requires_grad = False
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, ToTensor, Normalize
+import numpy as np
+from dataset.ImageToImageDataset import ImageToImageDataset
+
+def check_data(loader):
+    for images, labels in loader:
+        # get the first channel of labels
+        first_channel_labels = labels[:, 0, :, :]
+        print( "Labels Shape:", first_channel_labels.shape)
+        print("DAPI contain NaNs: ", torch.isnan(first_channel_labels).any().item())
+        print("DAPI contain Infs: ", torch.isinf(first_channel_labels).any().item())
+
+        '''
+        print("Images contain NaNs: ", torch.isnan(images).any().item())
+        print("Images contain Infs: ", torch.isinf(images).any().item())
+        print("Labels contain NaNs: ", torch.isnan(labels).any().item())
+        print("Labels contain Infs: ", torch.isinf(labels).any().item())
+        '''
+
+def main():
+    mean_data = [0.5, 0.5, 0.5]
+    std_data = [0.5, 0.5, 0.5]
+    mean_label = [0.5, 0.5, 0.5, 0.5]
+    std_label = [0.5, 0.5, 0.5, 0.5]
+
+    input_transform = Compose([
+        ToTensor(),
+        Normalize(mean=mean_data, std=std_data)
+    ])
+    label_transform = Compose([
+        ToTensor(),
+        Normalize(mean=mean_label, std=std_label)
+    ])
+
+    # Create instances of the ImageToImageDataset for the training, validation, and test sets
+    train_dataset = ImageToImageDataset(r'F:\2023_4_11_data_organization\224_patches\merged\small_dataset\train',
+                                        input_transform=input_transform, label_transform=label_transform)
+    val_dataset = ImageToImageDataset(r'F:\2023_4_11_data_organization\224_patches\merged\small_dataset\val',
+                                      input_transform=input_transform, label_transform=label_transform)
+    test_dataset = ImageToImageDataset(r'F:\2023_4_11_data_organization\224_patches\merged\small_dataset\test',
+                                       input_transform=input_transform, label_transform=label_transform)
+
+    # Create instances of the DataLoader for the training, validation, and test sets
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    print("Checking train loader...")
+    check_data(train_loader)
+    print("Checking val loader...")
+    check_data(val_loader)
+    print("Checking test loader...")
+    check_data(test_loader)
+
+if __name__ == '__main__':
+    main()
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,7 @@
 import torch
 from torchmetrics import StructuralSimilarityIndexMeasure
 import torch.nn.functional as F
+import numpy as np
 
 
 def calculate_ssim_per_channel(input_tensor, target_tensor):
@@ -17,6 +18,13 @@ def calculate_ssim_per_channel(input_tensor, target_tensor):
     for channel in range(input_tensor.size(1)):
         input_channel = input_tensor[:, channel, :, :].unsqueeze(1)
         target_channel = target_tensor[:, channel, :, :].unsqueeze(1)
+
+        # If the standard deviation of the input channel is zero, add a tiny value to the first pixel
+        if input_channel.std() == 0:
+            input_channel[0, 0, 0, 0] += 1e-8
+        if target_channel.std() == 0:
+            target_channel[0, 0, 0, 0] += 1e-8
+
         ssim_channel = ssim(input_channel, target_channel)
         ssim_channel = ssim_channel.mean()
         ssim_scores.append(ssim_channel.item())  # Convert to Python float right away
@@ -28,19 +36,36 @@ def calculate_pearson_corr(input_tensor, target_tensor):
     pearson_corr_scores = []
     psnr_scores = []
 
-    for channel in range (input_tensor.size(1)):
-        input_channel = input_tensor[:, channel, :, :].unsqueeze(1)
-        target_channel = target_tensor[:, channel, :, :].unsqueeze(1)
-        # Calculate mean squared error (MSE) and mean absolute error (MAE) between the output and target tensors
-        mse_loss = F.mse_loss(input_channel, target_channel)
-        mae_loss = F.l1_loss(input_channel, target_channel)
+    for channel in range(input_tensor.size(1)):
+        input_channel = input_tensor[:, channel, :, :].detach().cpu().numpy()
+        target_channel = target_tensor[:, channel, :, :].detach().cpu().numpy()
+
+        # If the standard deviation of the input channel is zero, add a tiny value to the first pixel
+        if input_channel.std() == 0:
+            input_channel[0, 0, 0, 0] += 1e-8
+        if target_channel.std() == 0:
+            target_channel[0, 0, 0, 0] += 1e-8
+
+        # Flatten the arrays
+        input_channel_flat = input_channel.flatten()
+        target_channel_flat = target_channel.flatten()
 
         # Calculate Pearson correlation coefficient
-        corr_coef = 1.0 - (2.0 * mae_loss) / (mse_loss + torch.mean(target_channel** 2))
+        corr_coef_matrix = np.corrcoef(input_channel_flat, target_channel_flat)
+
+        # np.corrcoef returns a 2D array, the correlation coefficient is at index [0,1] or [1,0]
+        corr_coef = corr_coef_matrix[0, 1]
+
+        pearson_corr_scores.append(corr_coef)
 
         # Calculate psnr
-        max_val = 1.0
-        psnr = 20 * torch.log10(max_val / torch.sqrt(mse_loss))
+        mse_loss = F.mse_loss(input_channel, target_channel)
+        if mse_loss < 1.0e-10:
+            psnr = torch.tensor(100.)
+        else:
+            # if the image is -1 to 1
+            max_val = 2.0
+            psnr = 20 * torch.log10(max_val / torch.sqrt(mse_loss))
 
         pearson_corr_scores.append(corr_coef.item())
         psnr_scores.append(psnr.item())
